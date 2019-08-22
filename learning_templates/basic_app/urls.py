8ba48265ce77c6435fd3a167c52855b76d4c459a
root <root@tpc000883.tgrc.tesco.org>
2019-08-22 11:07:32 +0100

    firstcommit

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from basic_app import views
+
+app_name = 'basic_app'
+
+urlpatterns = [
+
+      path(r'relative',views.relative,name='relative'),
+      path(r'other', views.other,name = 'other'),
+]
+
